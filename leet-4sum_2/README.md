0665ae4007871c7542e7d9a2548026ea8f3f15e5
Dushan <dusanterzic87@gmail.com>
2018-08-20 15:02:34 +0200

    modified:   leet-4sum_2/main.py

--- leet-4sum_2/README.md
+++ leet-4sum_2/README.md
@@ -2,4 +2,4 @@ Problem Name: 4Sum II
 Problem URL: https://leetcode.com/problems/4sum-ii/description/
 Author: Dushan Terzikj
 Author URL: https://dushanterzikj/in/dushanterzikj
-Version: 1.0.0
+Version: 2.0.0
--- leet-4sum_2/main.py
+++ leet-4sum_2/main.py
@@ -1,6 +1,33 @@
 import collections
 
 class Solution(object):
+
+    def to_num_count(self, A):
+        """
+        :type A: int
+        :rtype: dict(int, int)
+        """
+        ret = {}
+        for a in A:
+            if a not in ret:
+                ret[a] = 0
+            ret[a] += 1
+        return ret
+
+    def threeSumCount(self, A, B, num_sum):
+        """
+        :type A: dict(int, int)
+        :type B: dict(int, int)
+        :type num_sum: int
+        :rtype: int
+        """
+        count = 0
+        for i in A:
+            next_sum = num_sum - i
+            if next_sum in B:
+                count += B[next_sum]*A[i]
+        return count
+
     def fourSumCount(self, A, B, C, D):
         """
         :type A: List[int]
@@ -9,13 +36,20 @@ class Solution(object):
         :type D: List[int]
         :rtype: int
         """
-        ab = collections.Counter(a+b for a in A for b in B)
-        return sum(ab[-c-d] for c in C for d in D)
+        A = self.to_num_count(A)
+        B = self.to_num_count(B)
+        C = self.to_num_count(C)
+        D = self.to_num_count(D)
 
+        cd = {}
+        for c in C:
+            for d in D:
+                if c + d not in cd:
+                    cd[c + d] = 0
+                cd[c + d] += C[c]*D[d]
 
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-C = list(map(int, input().split()))
-D = list(map(int, input().split()))
-sol = Solution()
-print(sol.fourSumCount(A, B, C, D))
+        count = 0
+        for i in A:
+            count += self.threeSumCount(B, cd, -i) * A[i]
+        return count
+
